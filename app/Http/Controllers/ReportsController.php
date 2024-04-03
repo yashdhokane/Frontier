4b74ed260030a701be3c0457b5b1ea11f0add7c1
bawanesumit01 <bawanesumit01@gmail.com>
2024-04-03 17:35:01 +0530

    cc

--- app/Http/Controllers/ReportsController.php
+++ app/Http/Controllers/ReportsController.php
@@ -2,23 +2,154 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\ChatMessage;
+use App\Models\JobActivity;
+use App\Models\JobAssign;
+use App\Models\JobModel;
 use Illuminate\Http\Request;
 use App\Models\User;
 
 class ReportsController extends Controller
 {
     //techncian reorts
-   public function technicianreport(){
-    $technician = User::where('role', 'technician')->get();
-    return view('reports.technician', compact('technician'));
-}
+    public function technicianreport()
+    {
+        $technician = User::where('role', 'technician')->get();
 
-public function employeereport(){
-    $employees = User::whereNotIn('role', ['technician', 'customer'])->get();
-    return view('reports.employees', compact('employees'));
-}
-public function jobreport(){
-  
-    return view('reports.job');
-}
+        $job = JobModel::sum('gross_total');
+        $totalHours = JobAssign::sum('duration');
+        $totaldrivingHours = JobAssign::sum('driving_hours');
+        $jobcount = JobModel::count();
+
+        $grossTotalByTechnician = [];
+
+        foreach ($technician as $employee) {
+            // Sum of the gross_total column for jobs associated with the current employee
+            $totalGross = JobModel::where('technician_id', $employee->id)->sum('gross_total');
+            
+            // Store the total gross in the array with employee id as key
+            $grossTotalByTechnician[$employee->id] = $totalGross;
+        }
+
+        $jobCountsByTechnician = [];
+    
+        foreach ($technician as $employee) {
+            // Count the number of jobs associated with the current employee
+            $jobCount = JobModel::where('technician_id', $employee->id)->count();
+            
+            // Store the job count in the array with employee id as key
+            $jobCountsByTechnician[$employee->id] = $jobCount;
+        }
+
+        $jobHours = [];
+    
+        foreach ($technician as $employee) {
+            // Count the number of jobs associated with the current employee
+            $jobCount = JobAssign::where('technician_id', $employee->id)->sum('duration');
+            
+            // Store the job count in the array with employee id as key
+            $jobHours[$employee->id] = $jobCount;
+        }
+
+        $drivingHours = [];
+    
+        foreach ($technician as $employee) {
+            // Count the number of jobs associated with the current employee
+            $jobCount = JobAssign::where('technician_id', $employee->id)->sum('driving_hours');
+            
+            // Store the job count in the array with employee id as key
+            $drivingHours[$employee->id] = $jobCount;
+        }
+
+        return view('reports.technician', compact('technician','job','jobcount','grossTotalByTechnician','jobCountsByTechnician','totalHours','jobHours','totaldrivingHours','drivingHours'));
+    }
+
+    public function employeereport()
+    {
+        $employees = User::whereNotIn('role', ['technician', 'customer'])->get();
+    
+        
+        $job = JobModel::count();
+        // Initialize an array to store the job counts for each employee
+        $jobCountsByEmployee = [];
+    
+        foreach ($employees as $employee) {
+            // Count the number of jobs associated with the current employee
+            $jobCount = JobModel::where('added_by', $employee->id)->count();
+            
+            // Store the job count in the array with employee id as key
+            $jobCountsByEmployee[$employee->id] = $jobCount;
+        }
+
+        // Initialize an array to store the job counts for each employee
+        $jobCountsUpdatedBy = [];
+    
+        foreach ($employees as $employee) {
+            // Count the number of jobs associated with the current employee
+            $jobCount = JobModel::where('updated_by', $employee->id)->count();
+            
+            // Store the job count in the array with employee id as key
+            $jobCountsUpdatedBy[$employee->id] = $jobCount;
+        }
+    
+        // Initialize an array to store the job counts for each employee
+        $jobCountsClosedBy = [];
+    
+        foreach ($employees as $employee) {
+            // Count the number of jobs associated with the current employee
+            $jobCount = JobModel::where('closed_by', $employee->id)->count();
+            
+            // Store the job count in the array with employee id as key
+            $jobCountsClosedBy[$employee->id] = $jobCount;
+        }
+        
+        
+        $alltotalGross = JobModel::sum('gross_total');
+        $grossTotalByEmployee = [];
+
+        foreach ($employees as $employee) {
+            // Sum of the gross_total column for jobs associated with the current employee
+            $totalGross = JobModel::where('added_by', $employee->id)->sum('gross_total');
+            
+            // Store the total gross in the array with employee id as key
+            $grossTotalByEmployee[$employee->id] = $totalGross;
+        }
+        
+        
+        $totalActivity = JobActivity::count();
+        $activity = [];
+
+        foreach ($employees as $employee) {
+            // Sum of the gross_total column for jobs associated with the current employee
+            $activityCount = JobActivity::where('user_id', $employee->id)->count();
+            
+            // Store the total gross in the array with employee id as key
+            $activity[$employee->id] = $activityCount;
+        }
+
+
+        
+        $totalChats = ChatMessage::count();
+
+        $chats = [];
+
+        foreach ($employees as $employee) {
+            // Sum of the gross_total column for jobs associated with the current employee
+            $activityCount = ChatMessage::where('sender', $employee->id)->count();
+            
+            // Store the total gross in the array with employee id as key
+            $chats[$employee->id] = $activityCount;
+        }
+        
+
+    
+    
+        return view('reports.employees', compact('job','employees', 'jobCountsByEmployee','jobCountsUpdatedBy','jobCountsClosedBy','grossTotalByEmployee','alltotalGross','activity','totalActivity','chats','totalChats'));
+    }
+    
+    public function jobreport()
+    {
+
+        return view('reports.job');
+    }
 }
--- resources/views/reports/technician.blade.php
+++ resources/views/reports/technician.blade.php
@@ -2,17 +2,17 @@
 
 @section('content')
 
-<div class="page-wrapper" style="display:inline;">
+    <div class="page-wrapper" style="display:inline;">
         <!-- -------------------------------------------------------------- -->
         <!-- Bread crumb and right sidebar toggle -->
         <!-- -------------------------------------------------------------- -->
-		<div class="page-breadcrumb">
-			<div class="row">
-				<div class="col-5 align-self-center">
-					<h3 class="page-title">Technician Report</h3>
-				</div>
-  			</div>
-		</div>
+        <div class="page-breadcrumb">
+            <div class="row">
+                <div class="col-5 align-self-center">
+                    <h3 class="page-title">Technician Report</h3>
+                </div>
+            </div>
+        </div>
         <!-- -------------------------------------------------------------- -->
         <!-- End Bread crumb and right sidebar toggle -->
         <!-- -------------------------------------------------------------- -->
@@ -20,108 +20,130 @@
         <!-- Container fluid  -->
         <!-- -------------------------------------------------------------- -->
         <div class="container-fluid">
-          <div class="row">
-       
-              
-			  
-<div class="container">
-	
-	<div class="row">
-	
-		<div class="col-md-6">
-			<div class="card shadow card-border">
-				<div class="card-body">
-					<h4 class="card-title">Jobs completed by technician</h4>
-					<div class="row">
-						<div class="col-md-12">
-							<div class="table-responsive">
-								<table class="table customize-table mb-0 v-middle overflow-auto">
-								<thead class="table-dark">
-									<tr>
-										<th class="border-bottom border-top">Technician Name</th>
-										<th class="border-bottom border-top">Job Revenue</th>
-										<th class="border-bottom border-top">Job Count</th>
-										<th class="border-bottom border-top">Avg. Job Size</th>
-									</tr>
-									<tr class="table-success border-success">
-										<th class="border-bottom border-top">Total</th>
-										<th class="border-bottom border-top">$0</th>
-										<th class="border-bottom border-top">0</th>
-										<th class="border-bottom border-top">$0</th>
-									</tr>
-								</thead>
-								<tbody>
-        @foreach($technician as $user)
-        <tr>
-            <td>{{ $user->name ?? null }}</td>
-            <td>$0</td>
-            <td>0</td>
-            <td>$0</td>
-        </tr>
-        @endforeach
-    </tbody>
-								</table>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-		
-	
-		 <div class="col-md-6">
-			<div class="card shadow card-border">
-				<div class="card-body">
-					<h4 class="card-title">Time tracking (completed jobs)</h4>
-					<div class="row">
-						<div class="col-md-12">
-							<div class="table-responsive">
-								<table class="table customize-table mb-0 v-middle overflow-auto">
-								<thead class="table-dark">
-									<tr>
-										<th class="border-bottom border-top">Technician Name</th>
-										<th class="border-bottom border-top">Total On Job Hrs</th>
-										<th class="border-bottom border-top">Total Travel Hrs</th>
-										<th class="border-bottom border-top">Total Hrs Per Job</th>
-									</tr>
-									<tr class="table-success border-success">
-										<th class="border-bottom border-top">Total</th>
-										<th class="border-bottom border-top">0</th>
-										<th class="border-bottom border-top">0</th>
-										<th class="border-bottom border-top">0</th>
-									</tr>
-								</thead>
-								<tbody>
-        @foreach($technician as $user)
-        <tr>
-            <td>{{ $user->name ?? null }}</td>
-            <td>$0</td>
-            <td>0</td>
-            <td>$0</td>
-        </tr>
-        @endforeach
-    </tbody>
-								</table>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-		
-	</div>
-	 
-	 
-	
-</div>
+            <div class="row">
 
 
 
-			  
-              
+                <div class="container">
+
+                    <div class="row">
+
+                        <div class="col-md-6">
+                            <div class="card shadow card-border">
+                                <div class="card-body">
+                                    <h4 class="card-title">Jobs completed by technician</h4>
+                                    <div class="row">
+                                        <div class="col-md-12">
+                                            <div class="table-responsive">
+                                                <table class="table customize-table mb-0 v-middle overflow-auto">
+                                                    <thead class="table-dark">
+                                                        <tr>
+                                                            <th class="border-bottom border-top">Technician Name</th>
+                                                            <th class="border-bottom border-top">Job Revenue</th>
+                                                            <th class="border-bottom border-top">Job Count</th>
+                                                            <th class="border-bottom border-top">Avg. Job Size</th>
+                                                        </tr>
+                                                        <tr class="table-success border-success">
+                                                            <th class="border-bottom border-top">Total</th>
+                                                            <th class="border-bottom border-top">${{ $job }}</th>
+                                                            <th class="border-bottom border-top">{{ $jobcount }}</th>
+                                                            @if ($jobcount > 0)
+                                                                <td>${{ intval($job / $jobcount) }}
+                                                                </td>
+                                                            @else
+                                                                <td>N/A</td>
+                                                            @endif
+                                                        </tr>
+                                                    </thead>
+                                                    <tbody>
+                                                        @foreach ($technician as $user)
+                                                            <tr>
+                                                                <td>{{ $user->name ?? null }}</td>
+                                                                <td>${{ $grossTotalByTechnician[$user->id] }}</td>
+                                                                <td>{{ $jobCountsByTechnician[$user->id] }}</td>
+                                                                @if ($jobCountsByTechnician[$user->id] > 0)
+                                                                    <td>${{ intval($grossTotalByTechnician[$user->id] / $jobCountsByTechnician[$user->id]) }}
+                                                                    </td>
+                                                                @else
+                                                                    <td>N/A</td>
+                                                                @endif
+                                                            </tr>
+                                                        @endforeach
+                                                    </tbody>
+                                                </table>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+
+                        <div class="col-md-6">
+                            <div class="card shadow card-border">
+                                <div class="card-body">
+                                    <h4 class="card-title">Time tracking (completed jobs)</h4>
+                                    <div class="row">
+                                        <div class="col-md-12">
+                                            <div class="table-responsive">
+                                                <table class="table customize-table mb-0 v-middle overflow-auto">
+                                                    <thead class="table-dark">
+                                                        <tr>
+                                                            <th class="border-bottom border-top">Technician Name</th>
+                                                            <th class="border-bottom border-top">Total On Job Hrs</th>
+                                                            <th class="border-bottom border-top">Total Travel Hrs</th>
+                                                            <th class="border-bottom border-top">Total Hrs Per Job</th>
+                                                        </tr>
+                                                        <tr class="table-success border-success">
+                                                            <th class="border-bottom border-top">Total</th>
+                                                            <th class="border-bottom border-top">{{ $totalHours / 60 }}</th>
+                                                            <th class="border-bottom border-top">
+                                                                {{ $totaldrivingHours / 60 }}
+                                                            </th>
+                                                            @if ($totaldrivingHours > 0)
+                                                                <td>${{ intval($totalHours / $totaldrivingHours)  }}
+                                                                </td>
+                                                            @else
+                                                                <td>N/A</td>
+                                                            @endif
+                                                        </tr>
+                                                    </thead>
+                                                    <tbody>
+                                                        @foreach ($technician as $user)
+                                                            <tr>
+                                                                <td>{{ $user->name ?? null }}</td>
+                                                                <td>{{ $jobHours[$user->id] / 60 }}</td>
+                                                                <td>{{ $drivingHours[$user->id] / 60 }}</td>
+                                                                @if ($drivingHours[$user->id] > 0)
+                                                                    <td>{{ intval($jobHours[$user->id] / $drivingHours[$user->id]) }}
+                                                                    </td>
+                                                                @else
+                                                                    <td>N/A</td>
+                                                                @endif
+                                                            </tr>
+                                                        @endforeach
+                                                    </tbody>
+                                                </table>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+
+
+
+                </div>
+
+
+
+
+
             </div>
-          </div>
         </div>
-  </div>
+    </div>
+    </div>
 
-        @stop
+@stop
