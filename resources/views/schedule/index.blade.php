d62c47b8511439f1671b776e5e4d4abfb24ab931
sumit <bawanesumit01@gmail.com>
2024-05-11 09:13:11 +0530

    cc

--- resources/views/schedule/index.blade.php
+++ resources/views/schedule/index.blade.php
@@ -477,31 +477,6 @@
 
             <!-- Modal -->
 
-            <div class="modal fade" id="newCustomer" tabindex="-1" aria-labelledby="scroll-long-inner-modal"
-                aria-hidden="true">
-                <div class="modal-dialog modal-dialog-scrollable2 modal-dialog modal-xl">
-                    <div class="modal-content">
-                        <div class="modal-header d-flex align-items-center" style="padding-bottom: 0px;">
-                            <h4 class="modal-title" id="myLargeModalLabel" style="margin-left: 28px;color: #2962ff;">ADD
-                                NEW CUSTOMER
-                            </h4>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal"
-                                aria-label="Close"></button>
-                        </div>
-
-                        <div class="modal-body createCustomerData">
-
-                            @include('schedule.new_customer')
-
-                        </div>
-
-                    </div>
-
-
-
-                </div>
-            </div>
-
 
 
 
