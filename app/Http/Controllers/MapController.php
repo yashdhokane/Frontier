ee96c711d04d7af1270ea0c965d0d07ce88fdf3f
sumit <bawanesumit01@gmail.com>
2024-05-25 09:41:38 +0530

    cc

--- app/Http/Controllers/MapController.php
+++ app/Http/Controllers/MapController.php
@@ -14,12 +14,12 @@ class MapController extends Controller
 
     public function index(Request $request)
     {
-        
+
         $user_auth = auth()->user();
         $user_id = $user_auth->id;
         $permissions_type = $user_auth->permissions_type;
         $module_id = 31;
-        
+
         $permissionCheck =  app('UserPermissionChecker')->checkUserPermission($user_id, $permissions_type, $module_id);
         if ($permissionCheck === true) {
             // Proceed with the action
@@ -28,7 +28,7 @@ class MapController extends Controller
         }
 
 
-        $locationServiceSouthWest = DB::table('location_service_area')->where('area_name','South West')->first();
+        $locationServiceSouthWest = DB::table('location_service_area')->where('area_name', 'South West')->first();
 
         $query = DB::table('job_assigned')
             ->select(
@@ -62,17 +62,17 @@ class MapController extends Controller
 
         if (isset($request->area_id) && !empty($request->area_id)) {
             $query->where('jobs.service_area_id', $request->area_id);
-        }elseif (isset($locationServiceSouthWest->area_id) && !empty($locationServiceSouthWest->area_id)) {
+        } elseif (isset($locationServiceSouthWest->area_id) && !empty($locationServiceSouthWest->area_id)) {
             $query->where('jobs.service_area_id', $locationServiceSouthWest->area_id);
         }
 
         $data = $query->get();
 
-       
 
-        $locationServiceArea = DB::table('location_service_area')->where('area_type','region')->get();
 
-        return view('maps.index', compact('data', 'locationServiceArea','locationServiceSouthWest'));
+        $locationServiceArea = DB::table('location_service_area')->where('area_type', 'region')->get();
+
+        return view('maps.index', compact('data', 'locationServiceArea', 'locationServiceSouthWest'));
     }
 
     public function getMarkerDetails(Request $request)
@@ -108,27 +108,27 @@ class MapController extends Controller
             ->join('users as technician', 'technician.id', 'job_assigned.technician_id')
             ->where('job_assigned.job_id', $data['id'])->first();
 
-            $jobFieldsIds = explode(',', $result->job_field_ids);
+        $jobFieldsIds = explode(',', $result->job_field_ids);
 
-            $jobFields = DB::table('site_job_fields')
-                ->whereIn('field_id', $jobFieldsIds)
-                ->get();
+        $jobFields = DB::table('site_job_fields')
+            ->whereIn('field_id', $jobFieldsIds)
+            ->get();
 
-                $show_status = ''; 
+        $show_status = '';
 
-                foreach ($jobFields as $item) {
-                    $show_status .= '<span class="mb-1 badge bg-success">' . $item->field_name . '</span> '; 
-                }
-                if (empty($jobFields)) {
-                    $show_status = '<span class="mb-1 badge bg-secondary">No Fields</span>'; 
-                }
+        foreach ($jobFields as $item) {
+            $show_status .= '<span class="mb-1 badge bg-success">' . $item->field_name . '</span> ';
+        }
+        if (empty($jobFields)) {
+            $show_status = '<span class="mb-1 badge bg-secondary">No Fields</span>';
+        }
 
         $content = "
 			<div class='maplocationpopup'>
 			<h4 style='margin-bottom: 0px;'>" . $result->subject . "</h4>
 			<div class='mt-2'><span class='mb-1 badge bg-primary'>" . $result->job_id . "</span> " . $show_status . "</div>
 			<div class='mt-2'>" . $result->name . ", " . $result->address . ", " . $result->city . ", " . $result->state . ", " . $result->zipcode . "</div>
- 			<div class='mt-2'><a href='tickets/". $result->job_id ."' class='btn btn-success waves-effect waves-light btn-sm btn-info'>View</a> <a href='#' class='btn btn-warning waves-effect waves-light btn-sm btn-info reschedule' data-job_id='" . $result->job_id . "'>Reschedule</a></div>
+ 			<div class='mt-2'><a href='tickets/" . $result->job_id . "' class='btn btn-success waves-effect waves-light btn-sm btn-info'>View</a> <a href='#' class='btn btn-warning waves-effect waves-light btn-sm btn-info reschedule' data-job_id='" . $result->job_id . "'>Reschedule</a></div>
 		</div>";
 
         return response()->json(['content' => $content]);
@@ -151,7 +151,6 @@ class MapController extends Controller
                     $options .= "<option value='" . $value->id . "'>" . $value->name . "</option>";
                 }
             }
-
         }
 
         return $options;
@@ -159,7 +158,7 @@ class MapController extends Controller
 
     public function getJobDetails(Request $request)
     {
-      $locationServiceSouthWest = DB::table('location_service_area')->where('area_name','South West')->first();
+        $locationServiceSouthWest = DB::table('location_service_area')->where('area_name', 'South West')->first();
 
         $data = $request->all();
 
@@ -192,16 +191,15 @@ class MapController extends Controller
 
             $getData->start_date_time = $start_date_time->format('Y-m-d\TH:i');
 
-             $technician = null;
+            $technician = null;
 
-        if (isset($request->area_id) && !empty($request->area_id)) {
-            $technician = User::select('id', 'name')->where('role', 'technician')->where('service_areas', 'LIKE', '%' . $request->area_id . '%')->get();
-        }elseif (isset($locationServiceSouthWest->area_id) && !empty($locationServiceSouthWest->area_id)) {
-            $technician = User::select('id', 'name')->where('role', 'technician')->where('service_areas', 'LIKE', '%' . $locationServiceSouthWest->area_id . '%')->get();
-        }
+            if (isset($request->area_id) && !empty($request->area_id)) {
+                $technician = User::select('id', 'name')->where('role', 'technician')->where('service_areas', 'LIKE', '%' . $request->area_id . '%')->get();
+            } elseif (isset($locationServiceSouthWest->area_id) && !empty($locationServiceSouthWest->area_id)) {
+                $technician = User::select('id', 'name')->where('role', 'technician')->where('service_areas', 'LIKE', '%' . $locationServiceSouthWest->area_id . '%')->get();
+            }
 
-            return view('maps.reschedule_list', compact('getData', 'count','technician'));
-
+            return view('maps.reschedule_list', compact('getData', 'count', 'technician'));
         }
     }
 
@@ -210,94 +208,93 @@ class MapController extends Controller
 
         $data = $request->all();
         $timezone_name = Session::get('timezone_name');
+        $time_interval = Session::get('time_interval');
 
         try {
 
-        if (isset($data['technician_id']) && !empty($data['technician_id'])) {
+            if (isset($data['technician_id']) && !empty($data['technician_id'])) {
 
-            $technician_id = $data['technician_id'];
+                $technician_id = $data['technician_id'];
 
-            if (isset($data['rescheduleData']) && !empty($data['rescheduleData'])) {
+                if (isset($data['rescheduleData']) && !empty($data['rescheduleData'])) {
 
-                foreach ($data['rescheduleData'] as $key => $value) {
+                    foreach ($data['rescheduleData'] as $key => $value) {
 
-                    $start_date_time = Carbon::parse($value['start_date_time']);
+                        $newFormattedDateTime = Carbon::parse($value['start_date_time'])->subHours($time_interval)->format('Y-m-d H:i:s');
 
-                    $end_date_time = $start_date_time->copy()->addMinutes($value['duration']);
 
-                    $jobData = DB::table('jobs')->where('id', $value['job_id'])->update([
-                        'technician_id' => $technician_id,
-                        'updated_at' => date('Y-m-d H:i:s')
-                    ]);
+                        $start_date_time = Carbon::parse($newFormattedDateTime);
 
-                    $JobAssignedData = [
-                        'assign_status' => 'rescheduled',
-                    ];
+                        $end_date_time = $start_date_time->copy()->addMinutes($value['duration']);
 
-                    $jobAssignedID = DB::table('job_assigned')->where('job_id', $value['job_id'])->update($JobAssignedData);
+                        $jobData = DB::table('jobs')->where('id', $value['job_id'])->update([
+                            'technician_id' => $technician_id,
+                            'updated_at' => date('Y-m-d H:i:s')
+                        ]);
 
-                    $newJobAssignedData = [
-                        'technician_id' => $technician_id,
-                        'customer_id' => $value['customer_id'],
-                        'job_id' => $value['job_id'],
-                        'duration' => $value['duration'],
-                        'driving_hours' => $value['driving_hours'],
-                        'assign_title' => $value['assign_title'],
-                        'technician_note_id' => $value['technician_note_id'],
-                        'assign_description' => $value['assign_description'],
-                        'start_date_time' => $start_date_time->format('Y-m-d h:i:s'),
-                        'end_date_time' => $end_date_time->format('Y-m-d h:i:s'),
-                        'added_by' => auth()->id(),
-                        'updated_by' => auth()->id(),
-                        'created_at' => date('Y-m-d H:i:s'),
-                        'updated_at' => date('Y-m-d H:i:s'),
-                        'start_slot' => $start_date_time->format('H'),
-                        'end_slot' => $end_date_time->format('H'),
-                    ];
+                        $JobAssignedData = [
+                            'assign_status' => 'rescheduled',
+                        ];
 
-                    $newjobAssignedID = DB::table('job_assigned')->insertGetId($newJobAssignedData);
+                        $jobAssignedID = DB::table('job_assigned')->where('job_id', $value['job_id'])->update($JobAssignedData);
 
-                    $scheduleData = [
-                        'technician_id' => $technician_id,
-                        'start_date_time' => $start_date_time->format('Y-m-d h:i:s'),
-                        'end_date_time' => $end_date_time->format('Y-m-d h:i:s'),
-                        'updated_by' => auth()->id(),
-                        'updated_at' => date('Y-m-d H:i:s'),
-                        'start_slot' => $start_date_time->format('H'),
-                        'end_slot' => $end_date_time->format('H'),
-                    ];
+                        $newJobAssignedData = [
+                            'technician_id' => $technician_id,
+                            'customer_id' => $value['customer_id'],
+                            'job_id' => $value['job_id'],
+                            'duration' => $value['duration'],
+                            'driving_hours' => $value['driving_hours'],
+                            'assign_title' => $value['assign_title'],
+                            'technician_note_id' => $value['technician_note_id'],
+                            'assign_description' => $value['assign_description'],
+                            'start_date_time' => $start_date_time->format('Y-m-d h:i:s'),
+                            'end_date_time' => $end_date_time->format('Y-m-d h:i:s'),
+                            'added_by' => auth()->id(),
+                            'updated_by' => auth()->id(),
+                            'created_at' => date('Y-m-d H:i:s'),
+                            'updated_at' => date('Y-m-d H:i:s'),
+                            'start_slot' => $start_date_time->format('H'),
+                            'end_slot' => $end_date_time->format('H'),
+                        ];
 
-                    $schedule = DB::table('schedule')->where('job_id', $value['job_id'])->update($scheduleData);
-                
-                    $technician_name = User::where('id',$technician_id)->first();
-                    $now = Carbon::now($timezone_name);
-                    $formattedDate = $start_date_time->format('D, M j');
-                    $formattedTime = $now->format('g:ia');
-                    $formattedDateTime = "{$formattedDate} at {$formattedTime}";
-                    $activity ='Job Re-Scheduled for '. $formattedDateTime;
+                        $newjobAssignedID = DB::table('job_assigned')->insertGetId($newJobAssignedData);
 
-                    app('JobActivityManager')->addJobActivity($value['job_id'], $activity);
-                    app('sendNotices')(
-                        "Reschedule Job","Reschedule Job (#{$value['job_id']} - {$value['customer_name']}) added by {$technician_name->name}",
-                        url()->current(), 
-                        'job'
-                    );
+                        $scheduleData = [
+                            'technician_id' => $technician_id,
+                            'start_date_time' => $start_date_time->format('Y-m-d h:i:s'),
+                            'end_date_time' => $end_date_time->format('Y-m-d h:i:s'),
+                            'updated_by' => auth()->id(),
+                            'updated_at' => date('Y-m-d H:i:s'),
+                            'start_slot' => $start_date_time->format('H'),
+                            'end_slot' => $end_date_time->format('H'),
+                        ];
 
+                        $schedule = DB::table('schedule')->where('job_id', $value['job_id'])->update($scheduleData);
+
+                        $technician_name = User::where('id', $technician_id)->first();
+                        $now = Carbon::now($timezone_name);
+                        $formattedDate = $start_date_time->format('D, M j');
+                        $formattedTime = $now->format('g:ia');
+                        $formattedDateTime = "{$formattedDate} at {$formattedTime}";
+                        $activity = 'Job Re-Scheduled for ' . $formattedDateTime;
+
+                        app('JobActivityManager')->addJobActivity($value['job_id'], $activity);
+                        app('sendNotices')(
+                            "Reschedule Job",
+                            "Reschedule Job (#{$value['job_id']} - {$value['customer_name']}) added by {$technician_name->name}",
+                            url()->current(),
+                            'job'
+                        );
+                    }
                 }
-
             }
 
-        }
-
-        return 'true';
-
+            return 'true';
         } catch (\Exception $e) {
 
             Storage::append('Reschedule.log', ' error_msg -- ' . json_encode($e->getMessage()) . ' line number: ' . json_encode($e->getLine()) . ' File: ' . json_encode($e->getFile()) . ' - ' . date('Y-m-d H:i:s') . PHP_EOL);
 
             return 'false';
         }
-
     }
-
 }
