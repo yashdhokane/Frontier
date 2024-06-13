a6db63085a7d23a2cbba70fbbf8b0d91cc5572f4
sumit <bawanesumit01@gmail.com>
2024-06-13 15:34:47 +0530

    cc

--- app/Http/Controllers/CustomizerController.php
+++ app/Http/Controllers/CustomizerController.php
@@ -3,7 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Models\Customizer;
+use App\Models\Payment;
+use App\Models\Schedule;
+use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
 
 class CustomizerController extends Controller
 {
@@ -12,10 +17,29 @@ class CustomizerController extends Controller
      */
     public function index()
     {
+        $timezone_name = Session::get('timezone_name');
         $cardPositions = Customizer::orderBy('position')->get();
-        return view('customizer.dashboard', compact('cardPositions'));
+        $job = Schedule::with('JobModel', 'technician')
+            ->where('start_date_time', '>', Carbon::now($timezone_name))
+            ->latest()->limit(5)->get();
+
+        $paymentopen = Payment::with('JobAppliances', 'user', 'JobModel')->where('status', 'paid')->orderBy('id', 'desc')
+
+            ->limit(5)->get();
+
+        $paymentclose = Payment::with('JobAppliances', 'user', 'JobModel')->where('status', 'unpaid')->orderBy('id', 'desc')
+
+            ->limit(5)->get();
+
+
+        $adminCount = User::where('role', 'admin')->count();
+        $dispatcherCount = User::where('role', 'dispatcher')->count();
+        $technicianCount = User::where('role', 'technician')->count();
+        $customerCount = User::where('role', 'customer')->count();
+
+        return view('customizer.dashboard', compact('cardPositions','job','paymentopen','paymentclose','adminCount','dispatcherCount','technicianCount','customerCount'));
     }
-    
+
     public function savePositions(Request $request)
     {
         $positions = json_decode($request->positions, true);
--- resources/views/customizer/dashboard.blade.php
+++ resources/views/customizer/dashboard.blade.php
@@ -8,7 +8,7 @@
         <!-- -------------------------------------------------------------- -->
         <div class="row">
             <div class="col-12">
-                <h4 class="mb-3">Basic Draggable options</h4>
+                <h4 class="mb-3 page-title text-info fw-bold">DISPATCH CHANNEL INC.</h4>
                 <form id="positionForm" method="POST" action="{{ route('savePositions') }}">
                     @csrf
                     <input type="hidden" name="positions" id="positions">
@@ -19,14 +19,48 @@
                                 <div class="col-md-6 col-sm-12" data-id="{{ $cardPosition->element_id }}">
                                     <div class="card card-hover">
                                         <div class="card-header bg-info">
-                                            <h4 class="mb-0 text-white">Card Title 1</h4>
+                                            <h4 class="mb-0 text-white">Upcoming Job </h4>
                                         </div>
                                         <div class="card-body">
-                                            <h3 class="card-title">Special title treatment 1</h3>
-                                            <p class="card-text">
-                                                With supporting text below as a natural lead-in to additional content.
-                                            </p>
-                                            <a href="javascript:void(0)" class="btn btn-inverse">Go somewhere 1</a>
+                                            <div class="table-responsive mt-1" style="overflow-x: scroll !important;">
+                                                <table id="" class="table table-bordered text-nowrap">
+                                                    <thead class="uppercase">
+                                                        <tr>
+                                                            <th>Jobs Details</th>
+                                                            <th>Customer</th>
+                                                            <th>Technician</th>
+                                                            <th>Date</th>
+                                                        </tr>
+                                                    </thead>
+                                                    <tbody>
+                                                        @foreach ($job as $item)
+                                                            <tr>
+                                                                <td>
+                                                                    <a href="{{ url('tickets/' . $item->JobModel->id) }}"
+                                                                        class="font-medium link">{{ $item->JobModel->job_title ?? null }}</a><br />
+                                                                    {{ $item->JobModel->description ?? null }}
+                                                                </td>
+                                                                <td>{{ $item->JobModel->user->name ?? null }}</td>
+                                                                <td>{{ $item->technician->name ?? null }}</td>
+                                                                <td>
+                                                                    @if ($item && $item->start_date_time)
+                                                                        <div class="font-medium link ft12">
+                                                                            {{ $convertDateToTimezone($item->start_date_time ?? null) }}
+                                                                        </div>
+                                                                    @else
+                                                                        <div></div>
+                                                                    @endif
+                                                                    <div class="ft12">
+                                                                        {{ $convertTimeToTimezone($item->start_date_time ?? null, 'H:i:a') }}
+                                                                        to
+                                                                        {{ $convertTimeToTimezone($item->end_date_time ?? null, 'H:i:a') }}
+                                                                    </div>
+                                                                </td>
+                                                            </tr>
+                                                        @endforeach
+                                                    </tbody>
+                                                </table>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
@@ -34,14 +68,48 @@
                                 <div class="col-md-6 col-sm-12" data-id="{{ $cardPosition->element_id }}">
                                     <div class="card card-hover">
                                         <div class="card-header bg-danger">
-                                            <h4 class="mb-0 text-white">Card Title 2</h4>
+                                            <h4 class="mb-0 text-white">Open invoices</h4>
                                         </div>
                                         <div class="card-body">
-                                            <h3 class="card-title">Special title treatment 2</h3>
-                                            <p class="card-text">
-                                                With supporting text below as a natural lead-in to additional content.
-                                            </p>
-                                            <a href="javascript:void(0)" class="btn btn-inverse">Go somewhere 2</a>
+                                            <div class="table-responsive mt-1" style="overflow-x: scroll !important;">
+                                                <table id="" class="table table-bordered text-nowrap">
+                                                    <thead>
+                                                        <!-- start row -->
+                                                        <tr>
+                                                            <th>ID</th>
+
+                                                            <th>Customer</th>
+                                                            <th>Technician</th>
+                                                            <th>Inv. Date</th>
+                                                            <th>Due Date</th>
+                                                            <th>Amount</th>
+
+                                                        </tr>
+                                                        <!-- end row -->
+                                                    </thead>
+                                                    <tbody>
+                                                        <!-- start row -->
+                                                        @foreach ($paymentclose as $index => $item)
+                                                            <tr>
+                                                                <td><a
+                                                                        href="{{ url('invoice-detail/' . $item->id) }}">{{ $item->invoice_number ?? null }}</a>
+                                                                </td>
+
+                                                                <td>{{ $item->user->name ?? null }}</td>
+                                                                <td>{{ $item->JobModel->technician->name ?? null }}</td>
+                                                                <td>{{ $convertDateToTimezone($item->issue_date ?? null) }}
+                                                                </td>
+                                                                <td>{{ $item->due_date ?? null }}</td>
+                                                                <td>${{ $item->total ?? null }}</td>
+
+
+                                                            </tr>
+                                                            <!-- Modal for adding comment -->
+                                                        @endforeach
+
+                                                    </tbody>
+                                                </table>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
@@ -49,14 +117,48 @@
                                 <div class="col-md-6 col-sm-12" data-id="{{ $cardPosition->element_id }}">
                                     <div class="card card-hover">
                                         <div class="card-header bg-success">
-                                            <h4 class="mb-0 text-white">Card Title 3</h4>
+                                            <h4 class="mb-0 text-white">Paid invoices</h4>
                                         </div>
                                         <div class="card-body">
-                                            <h3 class="card-title">Special title treatment 3</h3>
-                                            <p class="card-text">
-                                                With supporting text below as a natural lead-in to additional content.
-                                            </p>
-                                            <a href="javascript:void(0)" class="btn btn-inverse">Go somewhere 3</a>
+                                            <div class="table-responsive mt-1" style="overflow-x: scroll !important;">
+                                                <table id="" class="table table-bordered text-nowrap">
+                                                    <thead>
+                                                        <!-- start row -->
+                                                        <tr>
+                                                            <th>ID</th>
+
+                                                            <th>Customer</th>
+                                                            <th>Technician</th>
+                                                            <th>Inv. Date</th>
+                                                            <th>Due Date</th>
+                                                            <th>Amount</th>
+
+                                                        </tr>
+                                                        <!-- end row -->
+                                                    </thead>
+                                                    <tbody>
+                                                        <!-- start row -->
+                                                        @foreach ($paymentopen as $index => $item)
+                                                            <tr>
+                                                                <td><a
+                                                                        href="{{ url('invoice-detail/' . $item->id) }}">{{ $item->invoice_number ?? null }}</a>
+                                                                </td>
+
+                                                                <td>{{ $item->user->name ?? null }}</td>
+                                                                <td>{{ $item->JobModel->technician->name ?? null }}</td>
+                                                                <td>{{ $convertDateToTimezone($item->issue_date ?? null) }}
+                                                                </td>
+                                                                <td>{{ $item->due_date ?? null }}</td>
+                                                                <td>${{ $item->total ?? null }}</td>
+
+
+                                                            </tr>
+                                                            <!-- Modal for adding comment -->
+                                                        @endforeach
+
+                                                    </tbody>
+                                                </table>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
@@ -64,14 +166,59 @@
                                 <div class="col-md-6 col-sm-12" data-id="{{ $cardPosition->element_id }}">
                                     <div class="card card-hover">
                                         <div class="card-header bg-warning">
-                                            <h4 class="mb-0 text-white">Card Title 4</h4>
+                                            <h4 class="mb-0 text-white">Stats</h4>
                                         </div>
                                         <div class="card-body">
-                                            <h3 class="card-title">Special title treatment 4</h3>
-                                            <p class="card-text">
-                                                With supporting text below as a natural lead-in to additional content.
-                                            </p>
-                                            <a href="javascript:void(0)" class="btn btn-inverse">Go somewhere 4</a>
+                                            <div class="row">
+                                                <div class="col-6 mb-3">
+                                                    <div class="text-white bg-primary rounded">
+                                                        <div class="card-body">
+                                                            <span><i class="ri-group-line"
+                                                                    style="font-size: 36px;"></i></span>
+                                                            <h3 class="card-title mt-1 mb-0 text-white">
+                                                                {{ $customerCount ?? null }}</h3>
+                                                            <p class="card-text text-white-50 fs-3 fw-normal">Customers</p>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div class="col-6 mb-3">
+                                                    <div class="text-white bg-warning rounded">
+                                                        <div class="card-body">
+                                                            <span><i class="ri-contacts-line"
+                                                                    style="font-size: 36px;"></i></span>
+                                                            <h3 class="card-title mt-1 mb-0 text-white">
+                                                                {{ $technicianCount ?? null }}</h3>
+                                                            <p class="card-text text-white-50 fs-3 fw-normal">Technicians
+                                                            </p>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="row">
+                                                <div class="col-6">
+                                                    <div class="text-white bg-success rounded">
+                                                        <div class="card-body">
+                                                            <span><i class="ri-admin-line"
+                                                                    style="font-size: 36px;"></i></span>
+                                                            <h3 class="card-title mt-1 mb-0 text-white">
+                                                                {{ $dispatcherCount ?? null }}</h3>
+                                                            <p class="card-text text-white-50 fs-3 fw-normal">Dispatchers
+                                                            </p>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div class="col-6">
+                                                    <div class="text-white bg-danger rounded">
+                                                        <div class="card-body">
+                                                            <span><i class="ri-admin-fill"
+                                                                    style="font-size: 36px;"></i></span>
+                                                            <h3 class="card-title mt-1 mb-0 text-white">
+                                                                {{ $adminCount ?? null }}</h3>
+                                                            <p class="card-text text-white-50 fs-3 fw-normal">Admin</p>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
