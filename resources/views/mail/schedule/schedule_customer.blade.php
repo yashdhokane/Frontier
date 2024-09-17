9bcffbe91dbf8037bcb87cce7fd462dec4a705b1
sumit <bawanesumit01@gmail.com>
2024-09-17 10:55:25 +0530

    cc

--- resources/views/mail/schedule/schedule_customer.blade.php
+++ resources/views/mail/schedule/schedule_customer.blade.php
@@ -4,6 +4,13 @@
     <table border="0" cellpadding="0" cellspacing="0" style="width: 100%">
       <tbody>
         <!-- start row -->
+        @php
+            // Retrieve time interval from the session
+            $time_interval = Session::get('time_interval');
+            
+            // Add the time interval to the start_date_time using Carbon
+            $newFormattedDateTimeAdd = \Carbon\Carbon::parse($maildata['data']['schedule']->start_date_time)->addHours($time_interval);
+        @endphp
         <tr>
           <td>
             <b>Hello {{ $maildata['data']['customer']->name ?? '' }},</b>
@@ -11,7 +18,7 @@
               We have 
               {{ $maildata['data']['typeName'] === 'schedule' ? 'scheduled' : 'rescheduled' }} 
               your service request. Our technician {{ $maildata['data']['technician']->name ?? '' }} will come on 
-              {{ \Carbon\Carbon::parse($maildata['data']['schedule']->start_date_time)->format('F j, Y, g:i a') }}.
+              {{ $newFormattedDateTimeAdd->format('F j, Y, g:i a') }}.
             </p>
           
             <b>- Thanks (Frontier team)</b>
