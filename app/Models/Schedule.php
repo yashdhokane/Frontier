e65901db3c831f2be296a294c196d98cdc46413f
sumit <bawanesumit01@gmail.com>
2025-03-20 14:47:13 +0530

    cc

--- app/Models/Schedule.php
+++ app/Models/Schedule.php
@@ -8,7 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Schedule extends Model
 {
     use HasFactory;
-    protected $table = 'schedule'; // Assuming your table name is 'service_category'
+    protected $table = 'schedule'; 
+    protected $primaryKey = 'id';
 
     protected $fillable = ['schedule_type', 'job_id', 'event_id', 'estimate_id', 'technician_id', 'start_date_time', 'end_date_time', 'remind_type', 'added_by', 'updated_by', 'start_slot', 'end_slot', 'timezone_id', 'show_on_schedule', 'position', 'is_routes_map', 'job_onmap_reaching_timing'];
 
